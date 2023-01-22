395c23742868daed75952bca95f24053bbbbcfbc
ajithkvp <ajithkvp.2010@gmail.com>
2023-01-21 19:15:07 -0500

    implemented add in NUM

--- src/HW2/num.py
+++ src/HW2/num.py
@@ -12,5 +12,14 @@ class NUM:
         self.hi = float("-inf")
         self.w = -1 if "-" in self.txt else 1
 
+    def add(self, n):
+        if not n =="?":
+            self.n += 1
+            d = n - self.mu
+            self.mu = self.mu + d / self.n
+            self.m2 = self.m2 + d * (n - self.mu)
+            self.lo = min(n, self.lo)
+            self.hi = max(n, self.hi)
+
 
 
