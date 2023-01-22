b8092442f52bbde6b9c4ef1a97efb95085649104
ajithkvp <ajithkvp.2010@gmail.com>
2023-01-21 19:14:41 -0500

    implemented constructor for NUM

--- src/HW2/num.py
+++ src/HW2/num.py
@@ -2,4 +2,15 @@ import math
 from utils import rnd
 
 class NUM:
-    pass
+    def __init__(self, at=None, txt=None):
+        self.at = at if at else 0
+        self.txt = txt if txt else ""
+        self.n = 0
+        self.mu = 0
+        self.m2 = 0
+        self.lo = float("inf")
+        self.hi = float("-inf")
+        self.w = -1 if "-" in self.txt else 1
+
+
+
