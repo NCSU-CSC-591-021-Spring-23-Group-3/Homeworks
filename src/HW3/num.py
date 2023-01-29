07cb55e0310fd40924cbfbb62c88d594a2d0344a
ajithkvp <ajithkvp.2010@gmail.com>
2023-01-29 18:32:15 -0500

    added add, mid and div function

--- src/HW3/num.py
+++ src/HW3/num.py
@@ -12,3 +12,20 @@ class NUM:
         self.hi = float('-inf')
         self.w = -1 if "-" in self.txt else 1
 
+    def add(self, n):
+        if n != "?":
+            self.n += 1
+            d = n - self.mu
+            self.mu += d / self.n
+            self.m2 += d * (n - self.mu)
+            self.lo = min(n, self.lo)
+            self.hi = max(n, self.hi)
+
+    def mid(self):
+        return self.mu
+
+    def div(self):
+        if (self.m2 < 0 or self.n < 2):
+            return 0
+        else:
+            return math.sqrt(self.m2 / (self.n - 1))
