0c5d27312f0c21d707346eb5d01d6a5b373a562f
ajithkvp <ajithkvp.2010@gmail.com>
2023-01-29 18:31:44 -0500

    implemented init for num

--- src/HW3/num.py
+++ src/HW3/num.py
@@ -2,4 +2,13 @@ import math
 from utils import rnd
 
 class NUM:
-    pass
+    def __init__(self, at=0, txt=""):
+        self.at = at
+        self.txt = txt
+        self.n = 0
+        self.mu = 0
+        self.m2 = 0
+        self.lo = float('inf')
+        self.hi = float('-inf')
+        self.w = -1 if "-" in self.txt else 1
+
